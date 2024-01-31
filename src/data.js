808a5824359d920167ac87a327d73763c9916fd9
krishna22241 <krishnapath224@gmail.com>
2024-01-31 12:02:31 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,55 @@
+const data = [
+    {
+        id:1,
+        name:"Agra",
+        info:"Agra division which consists of four districts, and is headed by the divisional commissioner of Agra, who is an IAS officer, the commissioner is the head of local government institutions (including municipal corporations) in the division, is in charge of infrastructure development in his division, and is also responsible for maintaining law and order in the division.[3][4][5][6] The district magistrate of Agra reports to the divisional commissioner.",
+        image:"https://cdn.thecodehelp.in/Agra.jpeg",
+        price:"35,758",
+    },
+    {
+        id:2,
+        name:"Jaipur",
+        info:"Jaipur is the capital of India’s Rajasthan state. It evokes the royal family that once ruled the region and that, in 1727, founded what is now called the Old City, or “Pink City” for its trademark building color. At the center of its stately street grid (notable in India) stands the opulent, colonnaded City Palace complex. With gardens, courtyards and museums, part of it is still a royal residence.",
+        image:"https://cdn.thecodehelp.in/Jaipur.jpeg",
+        price:"35,758",
+    },
+    {
+        id:3,
+        name:"Goa",
+        info:"Goa is a state in western India with coastlines stretching along the Arabian Sea. Its long history as a Portuguese colony prior to 1961 is evident in its preserved 17th-century churches and the area’s tropical spice plantations. Goa is also known for its beaches, ranging from popular stretches at Baga and Palolem to those in laid-back fishing villages such as Agonda. ",
+        image:"https://cdn.thecodehelp.in/Goa.jpeg",
+        price:"35,758",
+    },
+    {
+        id:4,
+        name:"Varanasi",
+        info:"Varanasi is a city in the northern Indian state of Uttar Pradesh dating to the 11th century B.C. Regarded as the spiritual capital of India, the city draws Hindu pilgrims who bathe in the Ganges River’s sacred waters and perform funeral rites. Along the city's winding streets are some 2,000 temples, including Kashi Vishwanath, the “Golden Temple,” dedicated to the Hindu god Shiva. ",
+        image:"https://cdn.thecodehelp.in/Varanasi.jpeg",
+        price:"35,758",
+    },
+    {
+        id:5,
+        name:"Darjeeling",
+        info:"Darjeeling is a town in India's West Bengal state, in the Himalayan foothills. Once a summer resort for the British Raj elite, it remains the terminus of the narrow-gauge Darjeeling Himalayan Railway, or “Toy Train,” completed in 1881. It's famed for the distinctive black tea grown on plantations that dot its surrounding slopes. Its backdrop is Mt. Kanchenjunga, among the world’s highest peaks.",
+        image:"https://cdn.thecodehelp.in/Darjeeling.jpeg",
+        price:"35,758",
+    },
+    {
+        id:6,
+        name:"Jaisalmer",
+        info:"Jaisalmer is a former medieval trading center and a princely state in the western Indian state of Rajasthan, in the heart of the Thar Desert. Known as the Golden City, it's distinguished by its yellow sandstone architecture. Dominating the skyline is Jaisalmer Fort, a sprawling hilltop citadel buttressed by 99 bastions. Behind its massive walls stand the ornate Maharaja's Palace and intricately carved Jain temples. ",
+        image:"https://cdn.thecodehelp.in/Jaisalmer.jpeg",
+        price:"35,758",
+    },
+    {
+        id:7,
+        name:"Ayodhya",
+        info:"Ayodhya is a city situated on the banks of the Sarayu river in the Indian state of Uttar Pradesh. It is the administrative headquarters of the Ayodhya district as well as the Ayodhya division of Uttar Pradesh, India. Ayodhya was historically known as Saketa.",
+        image:"https://media.assettype.com/outlooktraveller%2F2023-11%2F9ad2480a-206c-4d6e-b85b-95221af75863%2Fshutterstock_2338789429.jpg",
+        
+        price:"35,758",
+    },
+
+]
+
+export default data;
